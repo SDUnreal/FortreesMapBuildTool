00caea4ce4091ff0054162216a9da5d42bbb37ea
comwitch <tuna831@gmail.com>
2023-12-23 21:57:23 +0900

    add DesktopPlatform module

--- Source/FortressMapBuildTool/FortressMapBuildTool.Build.cs
+++ Source/FortressMapBuildTool/FortressMapBuildTool.Build.cs
@@ -8,7 +8,7 @@ public class FortressMapBuildTool : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" , "DesktopPlatform" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
