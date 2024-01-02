cfa3b058e9b6ab932a03f193c33b472d5e0e1e73
comwitch <tuna831@gmail.com>
2024-01-02 20:47:34 +0900

    fix something
    aaaaa

---
+++ Source/FortressMapBuildTool/Private/Voxel/VoxelArrayToHeightMap.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "VoxelArrayToHeightMap.generated.h"
+
+/**
+ * HeightMap to Voxel Array
+ *
+ * 
+ * 
+ * 
+ */
+
+
+
+
+UCLASS()
+class UVoxelArrayToHeightMap : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
